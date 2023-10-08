d2705c4384c9dcf573f2f5813d9635a33bba956f
M365Bass <bassem.nabil@gmail.com>
2023-10-09 00:12:08 +1100

    Auto #1: Package versioning in SPFx solutions

---
+++ automate.js
@@ -0,0 +1,47 @@
+var fs = require("fs");
+console.log("automate");
+
+const filePath1 =
+  "C:\\Users\\basse\\code\\testWPs\\VanillaForNode\\gulpfile.js";
+const source1 = "const build = require('@microsoft/sp-build-web');";
+const replacement1 =
+  'const gulp = require("gulp");\n' +
+  'const build = require("@microsoft/sp-build-web");';
+
+const source2 = "build.initialize(require('gulp'));";
+fs.readFile("./gulp", "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  replace(filePath1, [source1, source2], [replacement1, data]);
+
+  console.log("timeout 5s");
+  setTimeout(function () {
+    console.log("timeout 5s complete");
+    const execSync = require("child_process").execSync;
+    console.log(`npx prettier ${filePath1} --write`);
+    execSync(`npx prettier ${filePath1} --write`);
+  }, 5000);
+});
+
+function replace(filePath, source, replacement) {
+  console.log("replace");
+  fs.readFile(filePath1, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    if (source.length === replacement.length) {
+      var result = data.replace(source[0], replacement[0]);
+      for (let index = 1; index < source.length; index++) {
+        const sourceOfIndex = source[index];
+        const replacementOfIndex = replacement[index];
+        result = result.replace(source[index], replacement[index]);
+      }
+    }
+
+    fs.writeFile(filePath, result, "utf8", function (err) {
+      if (err) return console.log(err);
+    });
+  });
+}
