8eca888dac51754d089313efd9b921eb41e03ea2
M365Bass <bassem.nabil@gmail.com>
2023-10-09 22:14:28 +1100

    Updated afterEach to revert test changes to gulpfile.js

--- 1PkgVer.test.js
+++ 1PkgVer.test.js
@@ -1,21 +1,19 @@
 const fs = require("fs");
 const execSync = require("child_process").execSync;
 
+const path = ".\\testWPs\\TestSolution\\gulpfile.js";
 beforeEach(() => {
-  execSync(`node .\\1PkgVer.js -path .\\testWPs\\TestSolution\\gulpfile.js`);
+  execSync(`node .\\1PkgVer.js -path ${path}`);
 });
 
 test("File changed", () => {
-  const dataAfter = fs.readFileSync(
-    ".\\testWPs\\TestSolution\\gulpfile.js",
-    "utf8"
-  );
+  const dataAfter = fs.readFileSync(path, "utf8");
 
   const gulpDotJS = fs.readFileSync(".\\Sources\\gulp.js", "utf8");
 
   expect(dataAfter).toMatch(gulpDotJS);
 });
 
-// afterEach(() => {
-//   clearCityDatabase();
-// });
+afterEach(() => {
+  execSync(`git checkout --  ${path}`);
+});
