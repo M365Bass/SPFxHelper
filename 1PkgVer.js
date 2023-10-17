536311cfe79103d59463404af8d60dda746c02ac
M365Bass <bassem.nabil@gmail.com>
2023-10-18 00:06:53 +1100

    Updated PkgVer logic to remove npx prettier and simplify text replacement logic

--- 1PkgVer.js
+++ 1PkgVer.js
@@ -14,26 +14,26 @@ function PkgVer() {
   const options = program.opts();
 
   const gulpfilePath = options.Path;
-  const source1 = `const build = require("@microsoft/sp-build-web");`;
+  const source1 = `const build = require('@microsoft/sp-build-web');`;
   const replacement1 =
     'const gulp = require("gulp");\n' +
     'const build = require("@microsoft/sp-build-web");';
 
-  const source2 = 'build.initialize(require("gulp"));';
+  const source2 = `build.initialize(require('gulp'));`;
 
   const gulpSourceFile = path.join(__dirname, "\\Sources\\gulp.js");
-  fs.readFile(gulpSourceFile, "utf8", function (err, data1) {
+  fs.readFile(gulpSourceFile, "utf8", function (err, gulpSourceFileData) {
     if (err) return CHLK.ChalkError(err);
 
     const source = [source1, source2];
-    const replacement = [replacement1, data1];
+    const replacement = [replacement1, gulpSourceFileData];
 
-    fs.readFile(gulpfilePath, "utf8", function (err, data2) {
+    fs.readFile(gulpfilePath, "utf8", function (err, gulpfilePathData) {
       if (err) return CHLK.ChalkError(err);
 
       if (source.length === replacement.length) {
-        var result = data2.replace(source1, replacement1);
-        result2 = result.replace(source2, data1);
+        var result = gulpfilePathData.replace(source1, replacement1);
+        result2 = result.replace(source2, gulpSourceFileData);
       }
 
       fs.writeFile(gulpfilePath, result2, "utf8", function (err) {
