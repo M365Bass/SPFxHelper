776f9dce0deeaeb682cb32b0f3c149ad308d5e88
M365Bass <bassem.nabil@gmail.com>
2023-10-18 00:10:48 +1100

    Updated FastServe logic with more messages and consistent order of execution (path checks before logic)

--- 2FastServe.js
+++ 2FastServe.js
@@ -3,18 +3,9 @@ const { Command } = require("commander");
 const execSync = require("child_process").execSync;
 const { chdir, cwd } = require("node:process");
 const CHLK = require("./Utils/ChalkHelper");
+const path = require("path");
 
 function FastServe() {
-  CHLK.ChalkMessage(`npm list -g --depth=0`);
-  const npmGlobalPackages = execSync(`npm list -g --depth=0`).toString();
-
-  if (npmGlobalPackages.indexOf("spfx-fast-serve") === -1) {
-    CHLK.ChalkMessage(`npm install spfx-fast-serve -g`);
-    execSync(`npm install spfx-fast-serve -g`);
-  } else {
-    CHLK.ChalkMessage("Package already installed");
-  }
-
   const program = new Command();
 
   program
@@ -23,11 +14,23 @@ function FastServe() {
 
   const options = program.opts();
 
-  chdir(options.Path);
-  CHLK.ChalkMessage(cwd());
+  if (!fs.existsSync(path.join(options.Path, "fast-serve\\config.json"))) {
+    CHLK.ChalkMessage(`Running npm list -g --depth=0`);
+    const npmGlobalPackages = execSync(`npm list -g --depth=0`).toString();
 
-  CHLK.ChalkMessage(`spfx-fast-serve`);
-  execSync(`spfx-fast-serve`);
+    if (npmGlobalPackages.indexOf("spfx-fast-serve") === -1) {
+      CHLK.ChalkMessage(`Installing spfx-fast-serve globally`);
+      execSync(`npm install spfx-fast-serve -g`);
+    } else {
+      CHLK.ChalkMessage("spfx-fast-serve already installed globally");
+    }
+
+    chdir(options.Path);
+    CHLK.ChalkMessage(`Running spfx-fast-serve`);
+    execSync(`spfx-fast-serve`);
+  } else {
+    CHLK.ChalkWarning("spfx-fast-serve already configured");
+  }
 }
 
 module.exports = FastServe;
