a91d934b158cc5c76771c1394ab824fa84f48824
TheBass <bassem.nabil@gmail.com>
2023-10-30 19:14:56 +1100

    Fixed issue with sortPackage running twice

--- Commands/sortPackage.js
+++ Commands/sortPackage.js
@@ -2,18 +2,19 @@ const replaceInFile = require("../Utils/replaceInFile");
 const executeCommand = require("../Utils/executeCommand");
 
 module.exports = function (packageJSON_filePath) {
-  executeCommand(
-    "npx sort-package-json",
-    packageJSON_filePath,
-    "package.json is sorted!"
-  );
-
   replaceInFile(
     "npx sort-package-json",
     packageJSON_filePath,
     '"scripts": {',
     '"scripts": {\n' + '"postinstall": "npx sort-package-json",\n',
+    'postinstall": "npx sort-package-json',
     `gulpfile updated with npx sort-package-json`,
     `gulpfile already contains npx sort-package-json`
+  );
+
+  executeCommand(
+    "npx sort-package-json",
+    packageJSON_filePath,
+    "package.json is sorted!"
   );
 };
