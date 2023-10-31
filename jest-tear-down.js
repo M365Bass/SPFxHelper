fecc3a2d98f0ba6d2c8e4294d6f02b4a84978ee7
M365Bass <bassem.nabil@gmail.com>
2023-10-31 16:45:55 +1100

    Fixing tear-down EBUSY issue

--- jest-tear-down.js
+++ jest-tear-down.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+const os = require("os");
+
 const parentFolderPath = require("./Utils/folderPaths").parentFolderPath();
 
 module.exports = function () {
+  process.chdir(os.homedir());
+
   fs.rmSync(parentFolderPath, {
     recursive: true,
     force: true,
