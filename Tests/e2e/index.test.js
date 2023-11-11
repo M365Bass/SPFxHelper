4ae30e5637da74239bd2920d1160c0f7af255145
TheBass <bassem.nabil@gmail.com>
2023-11-12 01:23:20 +1100

    index e2e test

---
+++ Tests/e2e/index.test.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const os = require("os");
+const resolve = require("path").resolve;
+const execSync = require("child_process").execSync;
+const join = require("path").join;
+const parentFolderPath_IndexTest =
+  require("../../Utils/folderPaths").parentFolderPath_IndexTest();
+const wpFolderPath_IndexTest =
+  require("../../Utils/folderPaths").wpFolderPath_IndexTest();
+
+beforeEach(() => {
+  // save current working dir in to revert to it in teardown
+  process.env.initialPath_IndexTest = process.cwd();
+
+  fs.mkdirSync(parentFolderPath_IndexTest, { recursive: true });
+
+  process.chdir(parentFolderPath_IndexTest);
+  execSync(
+    `yo @microsoft/sharepoint --solution-name "VanillaSolution" --framework "react"` +
+      ` --component-type "webpart" --component-name "WebPart1" --skip-install --environment "spo" `,
+    { stdio: [] }
+  );
+
+  process.chdir(wpFolderPath_IndexTest);
+  execSync(`spfx-jack -v -f -g -s -p`);
+});
+
+test("indexTest: fast-serve folder created", () => {
+  console.log(process.cwd());
+
+  expect(
+    fs.existsSync(resolve(wpFolderPath_IndexTest, "fast-serve"))
+  ).toBeTruthy();
+});
+
+test("npmVersion: version-sync gulp task added", () => {
+  const gulpfilePath = join(wpFolderPath_IndexTest, "gulpfile.js");
+  const gulpfilePathData = fs.readFileSync(gulpfilePath, "utf8");
+  expect(gulpfilePathData).toContain(`gulp.task("version-sync"`);
+});
+
+test("gitInit: .git folder created", () => {
+  expect(fs.existsSync(resolve(wpFolderPath_IndexTest, ".git"))).toBeTruthy();
+});
+
+test("sortPackage: postinstall script added", () => {
+  const packageJsonPath = join(wpFolderPath_IndexTest, "package.json");
+  const packageJSONData = fs.readFileSync(packageJsonPath, "utf8");
+  expect(packageJSONData).toContain(`npx sort-package-json`);
+});
+
+afterEach(() => {
+  process.chdir(os.homedir());
+
+  fs.rmSync(parentFolderPath_IndexTest, {
+    recursive: true,
+    force: true,
+  });
+
+  // revert to initial working dir from setup
+  process.chdir(process.env.initialPath_IndexTest);
+});
--- Utils/folderPaths.js
+++ Utils/folderPaths.js
@@ -8,3 +8,11 @@ exports.parentFolderPath = function () {
 exports.wpFolderPath = function () {
   return join(this.parentFolderPath(), "VanillaSolution").toString();
 };
+
+exports.parentFolderPath_IndexTest = function () {
+  return join(os.homedir(), "spfx-jack-testing", "IndexTest").toString();
+};
+
+exports.wpFolderPath_IndexTest = function () {
+  return join(this.parentFolderPath_IndexTest(), "VanillaSolution").toString();
+};
