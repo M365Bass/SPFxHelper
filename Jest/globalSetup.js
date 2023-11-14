7f7c991a42b8c71e917c8f6e0234497c2e058e8f
M365Bass <bassem.nabil@gmail.com>
2023-11-14 21:59:04 +1100

    Revert chalk 5 because of ESM (attempt #3) (#77)
    * Revert chalk 5 because of ESM
    * spfx-fast-serve as devDependency
    * Added cache
    * Removed console.log(checked)
    * suppress npm WARN
    * Coverage on node 16 (to match local)
    * Queue coverage after tests succeed for pull requests

--- Jest/globalSetup.js
+++ Jest/globalSetup.js
@@ -26,10 +26,11 @@ module.exports = function () {
   } else if (process.version.startsWith("v18")) {
     packagesToInstallGlobally.push("@microsoft/generator-sharepoint@1.18");
   }
-  console.log("checked");
 
   packagesToInstallGlobally.forEach((package_name) => {
-    execSync(`npm list -g ${package_name} || npm install -g ${package_name}`);
+    execSync(
+      `npm list -g ${package_name} || npm install -g ${package_name} --silent`
+    );
     console.log(package_name);
   });
 
